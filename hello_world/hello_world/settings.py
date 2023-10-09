f9d9b6c955b0f75c494c31765c5daa2de0889c7f
Melissa Yvette Cavazos <109395704+melissacallis@users.noreply.github.com>
2023-10-09 13:12:22 -0500

    staticfile fixes

--- hello_world/hello_world/settings.py
+++ hello_world/hello_world/settings.py
@@ -131,8 +131,11 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+STATICFILES_DIRS = [
+    BASE_DIR / "hello_world" / "static",
+]
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = "/static/"
 
 
