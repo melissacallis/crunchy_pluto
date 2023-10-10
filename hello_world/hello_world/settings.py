548d7f030ad3cc4d053cd6a594bb12e07df2c565
Melissa Yvette Cavazos <109395704+melissacallis@users.noreply.github.com>
2023-10-09 20:33:09 -0500

    fixed static dir

--- hello_world/hello_world/settings.py
+++ hello_world/hello_world/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
@@ -140,6 +140,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static')
+]
 
 
 AUTHENTICATION_BACKENDS = ['core.backends.CustomAuthBackend',
